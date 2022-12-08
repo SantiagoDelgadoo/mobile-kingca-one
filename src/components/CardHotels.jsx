be58e09b4a36331a463ffaebab359fd7c8b51452
HernanC95 <cavalieri.h95@gmail.com>
2022-12-08 11:33:44 -0300

    Create Page Hotels

--- src/components/CardHotels.jsx
+++ src/components/CardHotels.jsx
@@ -1,59 +1,63 @@
 import React from "react";
 import { useState, useEffect } from "react";
-/* import SearchBar from "./FormText"; */
-/* import { Link as NavLink } from "react-router-dom"; */
 import { useDispatch, useSelector } from "react-redux";
 import hotelsAction from "../redux/actions/hotelsAction";
 import { View, Text } from "react-native";
 import { StyleSheet, Image, Pressable, TextInput } from "react-native";
-
+import { Picker } from "@react-native-picker/picker";
 export default function CardHotels() {
-   let [data, setData] = useState({ name: "", order: "" });
+  let [data, setData] = useState({ name: "", order: "ASC" });
   const listFiltered = useSelector((store) => store.hotelsReducer.listFiltered);
   const dispatch = useDispatch();
-/*   let inputSearch = (event) => {
-    setData({ ...data, name: event.value });
-  };
- */
-
-  let inputOrder = (event) => {
-    setData({ ...data, order: event.target });
-  };
   useEffect(() => {
     dispatch(hotelsAction.filterHotels(data));
-    console.log(data);
   }, [data]);
   return (
     <>
       <View className="filters">
         <View>
-           <TextInput style={styles.inputHotels} onChange={(newText) => setData({ ...data, name: newText })}
+          <TextInput
+            style={styles.inputHotels}
+            onChangeText={(newText) => setData({ ...data, name: newText })}
             type="search"
-            placeholder="Search Hotel" /> 
+            placeholder="Search Hotel"
+          />
         </View>
-{/*         <View>
-           <form action="" className="filter-select">
-            <select onChange={inputOrder}>
-              <option value="">Choose an option</option>
-              <option value="ASC">Ascending order</option>
-              <option value="DESC">Descending order</option>
-            </select>
-          </form> 
-        </View> */}
-      </View> 
+        <View>
+          <Picker
+          style={styles.inputHotels}
+            selectedValue={data.order}
+            onValueChange={(itemValue, itemIndex) =>
+              setData({ ...data, order: itemValue })
+            }
+          >
+            <Picker.Item label="Ascending order" value="ASC" />
+            <Picker.Item label="Descending order" value="DESC" />
+          </Picker>
+        </View>
+      </View>
 
       {listFiltered.map((place) => {
         return (
           <View style={styles.containerCards} key={place.id}>
-            <Text style={styles.subtituloCitiesCard}>{place.name}</Text>
-            <Image style={styles.cardImg} source={{ uri: place.photo[0] }} />
-            <View>
+            <Text style={styles.subtituloCitiesCard} key={place.id}>
+              {place.name}
+            </Text>
+            <Image
+              style={styles.cardImg}
+              source={{ uri: place.photo[0] }}
+              key={place.id}
+            />
+            <View key={place.id}>
               <Pressable
                 style={
                   styles.buttonDetailsCities
                 } /*  onPress={() => props.props.navigate("Details Hotel", place._id)} */
+                key={place.id}
               >
-                <Text style={styles.tituloDetails}>Details</Text>
+                <Text style={styles.tituloDetails} key={place.id}>
+                  Details
+                </Text>
               </Pressable>
             </View>
           </View>
@@ -69,12 +73,11 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     width: "100%",
-    height: 380,
+    height: 360,
     backgroundColor: "white",
     color: "black",
     borderRadius: 30,
     marginBottom: 30,
-    /*  boxShadow: "rgba(50, 50, 93, 0.25)", */
   },
   subtituloCitiesCard: {
     fontSize: 25,
@@ -83,8 +86,8 @@ const styles = StyleSheet.create({
     width: "100%",
     backgroundColor: "#2020209d",
     padding: 10,
-    borderTopRightRadius:30,
-    borderTopLeftRadius:30,
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
   },
   cardImg: {
     width: "100%",
@@ -102,8 +105,8 @@ const styles = StyleSheet.create({
     display: "flex",
     alignContent: "center",
     justifyContent: "center",
-    borderBottomRightRadius:30,
-    borderBottomLeftRadius:30,
+    borderBottomRightRadius: 30,
+    borderBottomLeftRadius: 30,
   },
   tituloDetails: {
     color: "white",
@@ -115,10 +118,10 @@ const styles = StyleSheet.create({
   inputHotels: {
     fontSize: 15,
     borderWidth: 2,
-    borderColor: '#ea5318',
+    borderColor: "#ea5318",
     padding: 8,
     borderRadius: 8,
-    marginBottom:20,
-    backgroundColor:"white",
+    marginBottom: 20,
+    backgroundColor: "white",
   },
 });
